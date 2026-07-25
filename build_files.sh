a0323ff1bc42a07b4b2fa92803a1967aab0e3979
ihsan7510 <ihsan7510@users.noreply.github.com>
2026-07-25 22:18:10 +0530

    Use local virtual environment in build script to avoid PEP 668 error

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+echo "Creating virtual environment..."
+python3 -m venv venv
+source venv/bin/activate
+
 echo "Installing dependencies..."
-python3 -m pip install -r requirements.txt
+pip install -r requirements.txt
 
 echo "Collecting static files..."
 python3 manage.py collectstatic --no-input --clear
+
+echo "Deactivating virtual environment..."
+deactivate
